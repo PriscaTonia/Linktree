ada0fe87bc1e16972e5ed8820d2c6bc51a9adc7d
PriscaTonia <priscaebube@gmail.com>
2022-11-04 04:34:30 +0100

    refactor: Changing the submit input to a button

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -9,30 +9,36 @@ const Contact = () => {
                     <p>Hi there, Contact me to ask me anything you have in mind.</p>
                 </header>
                 <form action="">
+                    {/* Name */}
                     <div className={styles.nameGroup}>
                         <div className={styles.firstname}>
                             <label htmlFor="firstname">First Name</label> 
-                            <input type="text" name='firstname' placeholder='Enter your first name'/>
+                            <input type="text" name='firstname' id='first_name' placeholder='Enter your first name'/>
                         </div>
                         <div className={styles.lastname}>
                             <label htmlFor="lastname">Last Name</label> 
-                            <input type="text" name='lastname' placeholder='Enter your last name' />
+                            <input type="text" name='lastname' id='last_name' placeholder='Enter your last name' />
                         </div>
                     </div>
+
+                    {/* Email */}
                     <div className={styles.email}>
                         <label htmlFor="email">Email</label> 
-                        <input type="email" name="email" placeholder='yourname@email.com' />
+                        <input type="email" name="email" id='email' placeholder='yourname@email.com' />
                     </div>
+                    {/* Message */}
                     <div className={styles.message}>
                         <label htmlFor="message">Message</label> 
-                        <textarea name="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible "></textarea>
+                        <textarea name="message" cols="30" rows="10" id='message' placeholder="Send me a message and I'll reply you as soon as possible... "></textarea>
                     </div>
+                    {/* Checkbox */}
                     <div className={styles.checkbox}>
                         <input type="checkbox" name="check"  />
                         <label htmlFor="check">You agree to providing your data to Onwudebelu Prisca Ebubechukwu who may contact you.</label>
                     </div>
+                    {/* Submit Button */}
                     <div className={styles.submitbtn}>
-                        <input type="submit" value="Send Message" />
+                       <button id='btn__submit'>Send Message</button>
                     </div>
                 </form>
             </div>
