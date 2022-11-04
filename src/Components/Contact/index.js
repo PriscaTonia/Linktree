f767634730d3c76c270ca25501360e66b812ef4d
PriscaTonia <priscaebube@gmail.com>
2022-11-04 04:18:14 +0100

    feat: Creating contact component style: Desktop styles for contact component

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -8,6 +8,33 @@ const Contact = () => {
                     <h1>Contact Me</h1>
                     <p>Hi there, Contact me to ask me anything you have in mind.</p>
                 </header>
+                <form action="">
+                    <div className={styles.nameGroup}>
+                        <div className={styles.firstname}>
+                            <label htmlFor="firstname">First Name</label> 
+                            <input type="text" name='firstname' placeholder='Enter your first name'/>
+                        </div>
+                        <div className={styles.lastname}>
+                            <label htmlFor="lastname">Last Name</label> 
+                            <input type="text" name='lastname' placeholder='Enter your last name' />
+                        </div>
+                    </div>
+                    <div className={styles.email}>
+                        <label htmlFor="email">Email</label> 
+                        <input type="email" name="email" placeholder='yourname@email.com' />
+                    </div>
+                    <div className={styles.message}>
+                        <label htmlFor="message">Message</label> 
+                        <textarea name="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible "></textarea>
+                    </div>
+                    <div className={styles.checkbox}>
+                        <input type="checkbox" name="check"  />
+                        <label htmlFor="check">You agree to providing your data to Onwudebelu Prisca Ebubechukwu who may contact you.</label>
+                    </div>
+                    <div className={styles.submitbtn}>
+                        <input type="submit" value="Send Message" />
+                    </div>
+                </form>
             </div>
         </div>
      );
